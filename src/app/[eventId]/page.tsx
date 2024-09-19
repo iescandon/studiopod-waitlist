84b479164fca7c3d5bd627c17e6f6d8d18aaf41e
iescandon <iescan4@gmail.com>
2024-09-18 20:49:30 -0500

    remove my agg call

--- src/app/[eventId]/page.tsx
+++ src/app/[eventId]/page.tsx
@@ -11,17 +11,18 @@ export async function generateStaticParams() {
 }
 
 export default async function EventDetails({ params }: { params: { eventId: string } }) {
-const event: AggEvent = await getEventWithUserSessions(params.eventId);
-console.log(event);
+  // const event: AggEvent = await getEventWithUserSessions(params.eventId);
+  // console.log(event);
   return (
     <main>
-      <h1>{event.name}</h1>
-      <p>{event.date}</p>
-      <ul>
-      {event?.waiting?.map((session: AggSession) => (
-          <li key={session._id}>{session.user.name}</li>
-      ))}
-    </ul>
+      EVENT DETAILS
+        {/* <h1>{event.name}</h1>
+        <p>{event.date}</p>
+        <ul>
+        {event?.waiting?.map((session: AggSession) => (
+            <li key={session._id}>{session.user.name}</li>
+        ))}
+        </ul> */}
     </main>
   )
 }
