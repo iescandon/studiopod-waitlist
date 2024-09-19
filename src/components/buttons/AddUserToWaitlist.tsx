d983b322265e501e3298f55e177241c0fa31b9dd
iescandon <iescan4@gmail.com>
2024-09-18 19:58:08 -0500

    another error fix

--- src/components/buttons/AddUserToWaitlist.tsx
+++ src/components/buttons/AddUserToWaitlist.tsx
@@ -1,19 +1,10 @@
 'use client'
-import { createUserSession } from "@/utils";
 
 interface ButtonProps {
 eventId: string,
 }
 
 export function AddUserToWaitlistButton( { eventId }: ButtonProps ) {
-  const addUserToWaitlist = async () => {
-    // save without +
-    const newUser = {
-      name: "Lucky",
-      phone: "17134567891"
-    };
-    await createUserSession(eventId, newUser);
-  }
 
   return (
     <button onClick={() => {console.log("add user to waitlist")}}>Add User</button>
